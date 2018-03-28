4f06a90ff9078b0735e30bfaa15500802b15b049
Luke Kozak <kozakluke@gmail.com>
2018-03-28 11:30:55 +0200

    Fix correct works with classnames package

--- src/Main.js
+++ src/Main.js
@@ -24,7 +24,7 @@ class Main
     processFile(file)
     {
         let source = this.compilation.assets[file].source(),
-            middle = source.indexOf('arguments.length;');
+            middle = source.indexOf('arguments.length; _i');
         while (middle !==-1)
         {
             const term1 = StringUtil.backSearch(source, '{', middle),
@@ -34,7 +34,7 @@ class Main
             if (term1 < begin && term2 < begin)
                 source = StringUtil.replace(source, 'arguments;', begin, end + 1);
             
-            middle = source.indexOf('arguments.length;', middle + 1);
+            middle = source.indexOf('arguments.length; _i', middle + 1);
         }
         
         this.compilation.assets[file] = new ConcatSource(source);
